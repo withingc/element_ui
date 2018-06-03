6209ede130180eb3b94750794bbde6b217102e84
withing <569071094@qq.com>
2018-06-03 18:47:09 +0800

    navigate

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
 
-    <el-row>
-      <el-col :span="8"><div class="grid-content bg-purple">哈哈哈哈哈哈哈哈</div></el-col>
-      <el-col :span="8"><div class="grid-content bg-purple-light">
-        <img src="./assets/logo.png">
-        <div>
-          <el-button @click="startHacking">Start</el-button>
-        </div>
+    <el-row type="flex">
+      <el-col :span="2"><div class="grid-content"><img src="./assets/assassin.jpg" height="60"></div></el-col>
+      <el-col :span="12"><div class="grid-content">
+        <div class="">WITHING</div>
       </div></el-col>
-      <el-col :span="8"><div class="grid-content bg-purple">哈哈哈哈哈哈哈哈</div></el-col>
+      <el-col :span="10"><div class="grid-content">
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" active-text-color="#00BFFF">
+          <el-menu-item index="1">withing的主页</el-menu-item>
+          <el-menu-item index="2">Games pictures</el-menu-item>
+          <el-menu-item index="3">Favorite</el-menu-item>
+        </el-menu>
+      </div></el-col>
     </el-row>
   </div>
+
 </template>
 
 <script>
@@ -34,4 +38,32 @@ export default {
   font-family: Helvetica, sans-serif;
   text-align: center;
 }
+</style>
+<style>
+  .el-row {
+    margin-bottom: 20px;
+  &:last-child {
+     margin-bottom: 0;
+   }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
